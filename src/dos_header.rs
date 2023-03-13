b22f1fbe824871e67413ed3472afc0d1580f7bc6
Steve <Steve@Steve.steve>
2023-03-12 23:28:44 -0500

    Fix this for later, brain

--- src/dos_header.rs
+++ src/dos_header.rs
@@ -12,10 +12,13 @@ pub fn make_dos_header(data: &[u8], mz_found: usize) -> DosHeader {
     header.ox3c_offset = LittleEndian::read_u32(&data[cur..cur + 4]) as usize;
     cur += 0x04;
     header.has_stub = check_for_stub(&data[cur..]);
-    if header.ox3c_offset != 0x80 {
+    cur += 0x40;
+    if header.ox3c_offset != cur {
         header.has_rich = true;
-        println!("{:#04X}", header.ox3c_offset);
-        header.rich_xor_key = get_rich_xor(&data[0x80..header.ox3c_offset])
+        let data_to_end = header.ox3c_offset / 4;
+        println!("{cur:#04X} {:#04X} {}", header.ox3c_offset, (header.ox3c_offset/4)/4);
+        header.rich_xor_key = get_rich_xor_key(&data[0x80..header.ox3c_offset]);
+        let test = get_rich_data(&data[cur..data_to_end], (data_to_end / 4) as u32, header.rich_xor_key);
     }
 
 
@@ -31,15 +34,28 @@ fn check_for_stub(data: &[u8]) -> bool {
     false
 }
 
-fn get_rich_xor(data: &[u8]) -> u32 {
-
-    //The word "Rich" in hex
-    //let rich:[u8;4] = 0x52696368_u32.to_be_bytes();
-
-
+fn get_rich_xor_key(data: &[u8]) -> u32 {
     let (offset, _) = data.iter()
         .enumerate()
         .find(|(_, item)| "Rich".as_bytes().contains(item) )
         .unwrap();
     BigEndian::read_u32(&data[offset+4..offset+8])
+}
+
+fn get_rich_data(data: &[u8], iters: u32, key: u32) -> u32 {
+    let key : [u8;4] = key.to_be_bytes();
+
+    let mut hold: [u8;4] = [0;4];
+
+    for i in 0..4 {
+        hold[i] = &data[i] ^ key[i];
+    }
+
+    dbg!(hold);
+
+    for _ in 3..iters {
+
+    }
+
+    0
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod dos_header;
 
 use clap::Parser;
 use std::fs::File;
+use std::process;
 use crate::coff_header::{check_for_mz, get_large_data_chunk};
 use crate::coff_header::enums::PEFILEERROR::NoMZinFile;
 use crate::dos_header::make_dos_header;
@@ -18,11 +19,12 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let success = File::open(&args.filename).unwrap_or_else(|_| panic!("Could not open file {}", &args.filename));
+    let success = File::open(&args.filename).unwrap_or_else(|_|{ println!("Could not open file {}", &args.filename); process::exit(1);});
+
     let chunk :Vec<u8> = get_large_data_chunk(success);
     let dos_header_data = &chunk[0..=254];
 
-    let mz_offset = check_for_mz(dos_header_data).unwrap_or_else(|_| { panic!("{}", NoMZinFile) });
+    let mz_offset = check_for_mz(dos_header_data).unwrap_or_else(|_| { println!("{}", NoMZinFile); process::exit(1); });
     let header_dos = make_dos_header(dos_header_data, mz_offset);
     println!("{header_dos}");
 
