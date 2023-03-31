d5f4825b3bfbacdeb0a8fcd0ec8a173c2b0d6c4d
Steve <Steve@Steve.steve>
2023-03-31 00:28:09 -0500

    This may be version 0.5.
    Tests are running smoothly for every file in the 64 PE folder.

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,52 @@ pub fn show_headers(dosheader: &DosHeader, coffheader: &CoffHeader, optheader: &
     println!("{}", optheader.DATADIRECTORIES);
     println!("{}", secheader);
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{env, fs, process};
+    use std::fs::File;
+    use std::io::{BufReader, Read};
+    use crate::coff_header::make_coff_header;
+    use crate::dos_header::{check_for_mz, make_dos_header};
+    use crate::optional_header::make_optional_header;
+    use crate::sections_header::make_section_header;
+
+    #[test]
+    fn test_lots() {
+        let paths = fs::read_dir(env::var("PEFILES64").unwrap()).unwrap()
+            .filter(|x| x.as_ref().unwrap().path().extension() == Some("dat".as_ref()));
+
+        for item in paths {
+            println!("Name: {}", item.as_ref().unwrap().path().display());
+            let mut reader = BufReader::new(File::open(item.unwrap().path()).unwrap());
+            let mut buffer = [0; 0x5000];
+            let _size_read = reader.read(&mut buffer);
+
+            let mz_offset = match check_for_mz(&buffer) {
+                Ok(offset) => offset,
+                Err(e) => {
+                    println!("{e}");
+                    process::exit(1)
+                }
+            };
+
+            let header_dos = make_dos_header(&buffer, mz_offset);
+
+            let mut cursor = header_dos.0.pe_offset + 4;
+
+            let header_coff = make_coff_header(&buffer, cursor);
+
+            cursor += 20;
+
+            let (header_opt, cursor) = make_optional_header(&buffer, cursor);
+
+            let secheader = make_section_header(&buffer, cursor, header_coff.HE_SECTIONS as usize);
+
+            //show_headers(&header_dos.0, &header_coff, &header_opt, &secheader);
+
+
+        }
+
+    }
+}
