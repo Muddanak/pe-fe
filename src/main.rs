cf3c635f1ce6848c0c92dd5e72fc0dc688d259a0
Muddanak <49950508+Muddanak@users.noreply.github.com>
2023-04-03 22:07:49 -0500

    Added in more options for clap.
    -a/--all - Show all headers
    
    -r/--rich - Show the Rich header sha256 hash
    
    -s/--section - Show the Sections header

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{BufReader, Read};
 use std::{io, process};
 
-use pe_fe::sections_header::make_section_header;
+use pe_fe::sections_header::{make_section_header, print_section_headers};
 use pe_fe::{lib as pefelib, show_headers};
 use pefelib::dos_header::{check_for_mz, make_dos_header, print_rich_sha256_hash};
 
@@ -11,16 +11,23 @@ use pefelib::coff_header::make_coff_header;
 use pefelib::optional_header::make_optional_header;
 
 #[derive(Parser)]
-#[command(name="pe-fe")]
-#[command(author="Muddanak")]
-#[command(version)]
-#[command(about="A PE file analyzer written in Rust")]
+#[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
+    ///Filename to analyze
     filename: String,
 
-    #[arg(short, long)]
-    show_rich_hash: bool,
+    #[arg(short, long="rich")]
+    ///Show the 'Rich' header hash
+    rich_hash: bool,
+
+    #[arg(short, long="section")]
+    ///Show the Sections header
+    section_header: bool,
+
+    #[arg(short = 'a', long = "all")]
+    ///Show all header info parsed
+    show_all: bool,
 }
 
 fn main() -> io::Result<()> {
@@ -49,9 +56,13 @@ fn main() -> io::Result<()> {
 
     let secheader = make_section_header(&buffer, cursor, header_coff.HE_SECTIONS as usize);
 
-    show_headers(&header_dos.0, &header_coff, &header_opt, &secheader);
+    show_headers(&header_dos.0, &header_coff, &header_opt);
 
-    if header_dos.0.has_rich {
+    if args.section_header.eq(&true) || args.show_all.eq(&true) {
+        print_section_headers(&secheader);
+    }
+
+    if header_dos.0.has_rich && (args.rich_hash.eq(&true) || args.show_all.eq(&true)) {
         print_rich_sha256_hash(&header_dos.0);
     }
 
